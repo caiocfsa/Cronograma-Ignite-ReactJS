35df9a1024b4ebdb669938a7e8606a8678e54382
Caio Fernando <caiocfsa@hotmail.com>
2021-03-11 08:46:15 -0300

    first commit

---
+++ Cronograma.js
@@ -0,0 +1,47 @@
+const estudo = {
+  Semana: {
+    Segunda: {
+      Estudos: '06:30 - 08:00',
+      Descanso: '20:00 - 22:00',
+      projetosPessoais: '18-30 - 20:00',
+      Trabalho: '08:00 - 18:00',  
+    },
+    Terça: {
+      Estudos: '06:30 - 08:00',
+      Descanso: '20:00 - 22:00',
+      projetosPessoais: '18-30 - 20:00',
+      Trabalho: '08:00 - 18:00',  
+    },
+    Quarta: {
+      Estudos: '06:30 - 08:00',
+      Descanso: '20:00 - 22:00',
+      projetosPessoais: '18-30 - 20:00',
+      Trabalho: '08:00 - 18:00',  
+    },
+    Quinta: {
+      Estudos: '06:30 - 08:00',
+      Descanso: '20:00 - 22:00',
+      projetosPessoais: '18-30 - 20:00',
+      Trabalho: '08:00 - 18:00',  
+    },
+    Sexta: {
+      Estudos: '06:30 - 08:00',
+      Descanso: '20:00 - 22:00',
+      projetosPessoais: '18-30 - 20:00',
+      Trabalho: '08:00 - 18:00',  
+    },
+    Sabado: {
+      Estudos: {},
+      Descanso: '17:00 - 22:00',
+      projetosPessoais: '09:00 - 16:00',
+      Trabalho: {},  
+    },
+    Domingo: {
+      Estudos: {},
+      Descanso: '09:00 - 22:00',
+      projetosPessoais: {},
+      Trabalho: {},  
+    },
+  }
+};
+
